fbba1e16eaf4c7cfec36bb96fe78e602df2f46e9
eziquexx <waftyann@gmail.com>
2024-10-09 14:06:54 +0900

    각 페이지 이동 링크 수정 및 message create 폼 작업 fin

--- src/main/java/com/mysite/practice/message/MessageController.java
+++ src/main/java/com/mysite/practice/message/MessageController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -11,31 +12,38 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-@RequestMapping("/messages")
+@RequestMapping("/")
 public class MessageController {
 
 	@Autowired
 	private MessageService messageService;
 	
 	// messageDetail
-	@GetMapping("/{id}")
-	@ResponseBody
-	public MessageDTO getMessage(@PathVariable("id") Integer id) {
-		return messageService.getMessage(id);
+	@GetMapping("/messages/{id}")
+	public String getMessage(@PathVariable("id") Integer id, Model model) {
+		MessageDTO message = messageService.getMessage(id);
+		model.addAttribute("msg", message);
+		return "messageDetail";
 	}
 	
-	
 	// messageListPage
-	@GetMapping
+	@GetMapping("/messages")
 	public String getMessageList() {
 		return "messageList";
 	}
 	
 	// messageListAPI
-	@GetMapping("/api")
+	@GetMapping("/messages/api")
 	@ResponseBody
 	public List<MessageDTO> getMessageListAll() {
 		return messageService.getMessageListAll();
 	}
+	
+	// messageInsert
+	@GetMapping("/create")
+	public String getMessageCreate() {
+		return "messageCreate";
+	}
+	
 	
 }
